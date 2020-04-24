23338c502e26abb8b318dcfe9bdb6805b90aacec
Piyush Sharma <piyush_sharma5@apple.com>
2020-04-24 14:07:23 -0700

    hide indicators

--- Paging/HomeCollectionController.swift
+++ Paging/HomeCollectionController.swift
@@ -14,6 +14,7 @@ class HomeCollectionController: UICollectionViewController, UICollectionViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .green
+        collectionView.showsHorizontalScrollIndicator = false
         setup()
     }
     
